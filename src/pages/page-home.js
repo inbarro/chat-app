45a5141bb081d0023fdb528c7b0101eb883c06ce
Inbar Rosenblum <inbarrosenblum@gmail.com>
2021-06-29 20:38:17 +0300

    Update page-home.js

--- src/pages/page-home.js
+++ src/pages/page-home.js
@@ -4,14 +4,33 @@ import { urlForName } from '../router';
 import { PageElement } from '../helpers/page-element';
 import './page-chat'
 
-
 export class PageHome extends PageElement {
 
+  constructor()
+  {
+    super()
+    this.items = [{name: 1, val: 1}, {name:2, val:2}]
+  }
+
+
+//   render() {
+//     return html`
+//        <vaadin-vertical-layout>
+//          ${this.items.map(item => html`<div class="block">${item.name}: ${item.val}</div>`)}
+//        </vaadin-vertical-layout>
+//
+//
+// `;
+//   }
+
+
   render() {
-    return html` 
+    return html`
         <page-chat colour = black class="page-chat"></page-chat>
        `;
   }
+
+
 }
 
 customElements.define('page-home', PageHome);
